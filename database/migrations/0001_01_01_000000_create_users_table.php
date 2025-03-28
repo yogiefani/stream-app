8d115918be213296995ffb3e93877508d0a71fee
yogiefani <yogiefaniyc@gmail.com>
2025-03-28 22:19:26 +0700

    feat: add user migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->string('avatar')->nullable();
+            $table->enum('role', ['admin', 'member']);
             $table->rememberToken();
             $table->timestamps();
         });
